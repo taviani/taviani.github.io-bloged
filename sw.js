9934765744e281ddea6e7dc0229a3ed6023d538b
taviani <1995427+taviani@users.noreply.github.com>
2019-01-20 23:49:31 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-35dea5269ed7a3da2ce6.js"
+    "url": "webpack-runtime-64758f64ac0e8bcbaff5.js"
   },
   {
     "url": "app.cb0a039ae75e95e42b08.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3abea72fabbba3c529eb28bd02106f64"
+    "revision": "ce411d8260f8526c739f53bc89fd84bf"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
