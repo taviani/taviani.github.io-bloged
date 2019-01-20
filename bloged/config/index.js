f26cc40f694ed58976f198bf5e5588d28831258d
taviani <1995427+taviani@users.noreply.github.com>
2019-01-20 22:26:59 +0100

    bloged sources

---
+++ bloged/config/index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  pathPrefix: '/bloged', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'Edouard Lacan', // Navigation and Site Title
+  siteTitleAlt: 'Edouard Lacan - Mini Blog', // Alternative Site title for SEO
+  siteTitleManifest: 'Edouard Lacan',
+  siteUrl: 'https://taviani.github.io', // Domain of your site. No trailing slash!
+  siteLanguage: 'fr', // Language Tag on <html> element
+  siteHeadline: 'Macro blogging', // Headline for schema.org JSONLD
+  siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
+  siteDescription: 'Petit blog pour évoquer les sujets qui m&#130;intéressent, personnels ou professionnels', // Your site description
+  author: 'E Lacan', // Author for schemaORGJSONLD
+  siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  //userTwitter: '@minimal', // Twitter Username - Optional
+  //ogSiteName: 'minimal', // Facebook Site Name - Optional
+  //ogLanguage: 'en_US', // Facebook Language
+
+  // Manifest and Progress color
+  // See: https://developers.google.com/web/fundamentals/web-app-manifest/
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+
+  linkedin: 'https://www.linkedin.com/in/edouard-lacan/',
+  github: 'https://github.com/taviani',
+  instagram: 'https://www.instagram.com/edouardlacan/',
+}
