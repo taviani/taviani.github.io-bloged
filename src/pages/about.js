369208544b20346b30644e3482ecab9f3ca32b84
taviani <1995427+taviani@users.noreply.github.com>
2019-04-29 00:35:56 +0200

    conformité codacy

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { Link, graphql } from 'gatsby'
+import { Link } from 'gatsby'
 import styled from 'styled-components'
 // import kebabCase from 'lodash/kebabCase'
 
