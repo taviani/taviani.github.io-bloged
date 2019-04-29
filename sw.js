6d6aba0b1b38edd4cabe51e7d0688525596f1fc5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-29 12:58:34 +0000

    Deploy taviani/bloged to github.com/taviani/bloged.git:gh-pages

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-89deb6d6925aacc903d3.js"
+    "url": "webpack-runtime-f099ad9cff0e0ecd72e4.js"
   },
   {
     "url": "app.88019ef8d0d70af92720.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "de99365490ea4318db2a92c6622c7f20"
+    "revision": "45a4684a78137b4162ef22f545385eaf"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
